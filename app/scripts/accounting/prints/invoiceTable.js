ba5f03074673087b0a28c4314e94599f0f6727d5
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-06 23:20:03 +0200

    Working on views.

--- app/scripts/accounting/prints/invoiceTable.js
+++ app/scripts/accounting/prints/invoiceTable.js
@@ -17,8 +17,6 @@ angular.module('mt.accounting')
         },
         templateUrl: 'views/prints/invoiceTable.html',
         link: function(scope, element, attrs) {
-          //var props = scope.$eval(attrs.myComponentScope);
-
           angular.forEach(props, function(value, key) {
             scope[key] = value;
           });
