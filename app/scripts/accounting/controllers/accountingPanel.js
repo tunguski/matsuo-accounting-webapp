a72b7dc9c6b708a5240beeb1960566dd14251900
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-09 00:41:49 +0200

    Added easy pie chart to dependencies. Working on using it to present data.

--- app/scripts/accounting/controllers/accountingPanel.js
+++ app/scripts/accounting/controllers/accountingPanel.js
@@ -10,4 +10,41 @@
 angular.module('mt.accounting')
     .controller('AccountingPanelCtrl', function ($scope, $http, $location, CashRegister, CashRegisterReport) {
       $scope.setTitle("Panel administracji księgowej");
+
+      $scope.firstPercent = 32;
+      $scope.firstOptions = {
+        animate:{
+          duration:1000,
+          enabled:true
+        },
+        barColor:'#2C3E50',
+        scaleColor:'#aaa',
+        lineWidth:4,
+        lineCap:'circle'
+      };
+
+      $scope.secondPercent = 44;
+      $scope.secondOptions = {
+        animate:{
+          duration:1000,
+          enabled:true
+        },
+        barColor:'#ef1e25',
+        scaleColor:'#aaa',
+        lineWidth:4,
+        lineCap:'circle'
+      };
+
+      $scope.thirdPercent = 17;
+      $scope.thirdOptions = {
+        animate:{
+          duration:1000,
+          enabled:true
+        },
+        barColor:'#2C3E50',
+        scaleColor:'#aaa',
+        lineWidth:4,
+        lineCap:'circle'
+      };
+
     });
