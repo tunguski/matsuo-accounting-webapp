42f03f19f0483ecac566ea7821a9cf4c10a23e36
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-12 20:35:02 +0200

    Improving accounting visual side.

---
+++ app/scripts/accounting/controllers/accountingConfiguration.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mt.accounting.controller:AccountingConfigurationCtrl
+ * @description
+ * # AccountingConfigurationCtrl
+ * Controller of the mt.accounting
+ */
+angular.module('mt.accounting')
+    .controller('AccountingConfigurationCtrl', function ($scope, $http, CashRegister, User, OrganizationUnit) {
+      $scope.configuration = {
+        manyCashRegisters: false,
+        divisionsEnabled: false
+      };
+
+      CashRegister.query({}, function (cashRegisters) {
+        $scope.cashRegisters = cashRegisters;
+      });
+
+      User.query({}, function (users) {
+        $scope.users = users;
+      });
+
+      OrganizationUnit.query({}, function (organizationUnits) {
+        $scope.organizationUnits = organizationUnits;
+      });
+    });
+
