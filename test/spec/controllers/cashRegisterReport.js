b21d9168516882b95c7de9c12176d8136b5ff395
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-14 13:06:57 +0200

    New tests.

--- test/spec/controllers/cashRegisterReport.js
+++ test/spec/controllers/cashRegisterReport.js
@@ -3,13 +3,79 @@
 describe('CashRegisterReportCtrl', function () {
   var controller;
 
-  beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
-    controller = $controller('CashRegisterReportCtrl', { $scope: scope });
-  }));
+  describe('basic', function () {
+    beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
+      controller = $controller('CashRegisterReportCtrl', { $scope: scope });
+    }));
 
 
-  it('', function () {
-    scope.$digest();
+    it('save', function () {
+      scope.entity = {
+        cashRegister: {
+        },
+        prints: [
+          { fields: { cashRegisterAmount: 11 } },
+          { fields: { cashRegisterAmount: 23 } }
+        ]
+      };
 
+      expect(scope.sumCashDocuments()).toBe(34);
+    });
+
+
+    it('save', function () {
+      scope.entity = {
+        cashRegister: {
+
+        },
+        $save: function (fn) {
+          fn({}, function () { return '/prints/cashDocument/11'; });
+        }
+      };
+
+      scope.save();
+    });
+  });
+
+
+  describe('with idEntity', function () {
+    beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
+      var routeParams = {
+        idEntity: 11
+      };
+      controller = $controller('CashRegisterReportCtrl', { $scope: scope, $routeParams: routeParams });
+    }));
+
+
+    it('load', function () {
+      http.expectGET('/api/cashRegisterReports/11').respond(angular.toJson({
+        id: 11,
+        prints: [
+          { printClass: 'com.test.Print' },
+          { printClass: 'com.test.Print' }
+        ]
+      }));
+
+      http.flush();
+    });
+  });
+
+
+  describe('with idCashRegister', function () {
+    beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
+      var routeParams = {
+        idCashRegister: 11
+      };
+      controller = $controller('CashRegisterReportCtrl', { $scope: scope, $routeParams: routeParams });
+    }));
+
+
+    it('load', function () {
+      http.expectGET('/api/cashRegisterReports/reportForCashRegister/11').respond(angular.toJson({
+        id: 11
+      }));
+
+      http.flush();
+    });
   });
 });
