816d09b4375400dbee4f7635ea2e13197670675e
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-29 17:17:38 +0200

    Fix accounting prints shortcuts.

--- app/scripts/accounting.js
+++ app/scripts/accounting.js
@@ -2,9 +2,9 @@
 
 angular.module('mt.accounting', ['mt.webapp', 'mt.cashRegisterService'])
     .run(function (mtFormConfig) {
-      mtFormConfig.invoice = 'pl.matsuo.accounting.model.print.Invoice';
-      mtFormConfig.accountingPrint = 'pl.matsuo.accounting.model.print.AccountingPrint';
-      mtFormConfig.depositSlip = 'pl.matsuo.accounting.model.print.DepositSlip';
+      mtFormConfig.namesMap.invoice = 'pl.matsuo.accounting.model.print.Invoice';
+      mtFormConfig.namesMap.accountingPrint = 'pl.matsuo.accounting.model.print.AccountingPrint';
+      mtFormConfig.namesMap.depositSlip = 'pl.matsuo.accounting.model.print.DepositSlip';
     })
     .config(function (restFactoryProvider) {
       restFactoryProvider
