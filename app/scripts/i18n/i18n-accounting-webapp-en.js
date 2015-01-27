a11d5b01a15dc43fa8f32c05470ddd9e32e8968c
Marek Romanowski <marek.romanowski@gmail.com>
2015-01-27 20:47:59 +0100

    Translations.

--- app/scripts/i18n/i18n-accounting-webapp-en.js
+++ app/scripts/i18n/i18n-accounting-webapp-en.js
@@ -4,5 +4,63 @@ angular.module('mt.accounting')
     .config(function($translateProvider) {
       // english translation
       $translateProvider.translations('en', {
+
+        cashDocument: {
+          shortName: {
+            invoice: 'Inv.',
+            invoice_receipt: 'Rec.',
+            //withdrawSlip: 'KW',
+            //depositSlip: 'KP',
+            correctiveInvoice: 'Corr. inv.',
+            correctiveInvoice_receipt: 'Corr rec.',
+            generalPrint: 'General',
+            recipe: 'Recipe'
+          },
+
+          title: {
+            invoice: 'Invoice details',
+            invoice_receipt: 'Receipt details',
+            //withdrawSlip: 'Szczegóły KW',
+            //depositSlip: 'Szczegóły KP',
+            correctiveInvoice: 'Corrective invoice details',
+            correctiveInvoice_receipt: 'Corrective receipt details',
+            recipe: 'Recipe'
+          },
+
+          newTitle: {
+            invoice: 'Creating new invoice',
+            invoice_receipt: 'Creating new receipt',
+            //withdrawSlip: 'Tworzenie nowego KW',
+            //depositSlip: 'Tworzenie nowego KP',
+            correctiveInvoice: 'Creating new corrective invoice',
+            correctiveInvoice_receipt: 'Creating new corrective receipt'
+          },
+
+          saved: {
+            invoice: 'Invoice created',
+            invoice_receipt: 'Receipt created',
+            //withdrawSlip: 'Utworzono KW',
+            //depositSlip: 'Utworzono KP',
+            correctiveInvoice: 'Corrective invoice created',
+            correctiveInvoice_receipt: 'Corrective receipt created'
+          },
+
+          updated: {
+            invoice: 'Invoice changes saved',
+            invoice_receipt: 'Receipt changes saved',
+            //withdrawSlip: 'Zapisano zmiany KW',
+            //depositSlip: 'Zapisano zmiany KP',
+            correctiveInvoice: 'Corrective invoice changes saved',
+            correctiveInvoice_receipt: 'Corrective receipt changes saved'
+          },
+
+          positions: {
+            invoice_receipt: 'Receipt positions',
+            invoice: 'Invoice positions',
+            corrective_receipt: 'Positions after corrections',
+            corrective: 'Positions after corrections'
+          }
+        }
+
       });
     });
--- app/scripts/i18n/i18n-accounting-webapp-pl.js
+++ app/scripts/i18n/i18n-accounting-webapp-pl.js
@@ -4,5 +4,63 @@ angular.module('mt.accounting')
     .config(function($translateProvider) {
       // polish translation
       $translateProvider.translations('pl', {
+
+        cashDocument: {
+          shortName: {
+            invoice: 'FV',
+            invoice_receipt: 'Par.',
+            withdrawSlip: 'KW',
+            depositSlip: 'KP',
+            correctiveInvoice: 'FV koryg.',
+            correctiveInvoice_receipt: 'Par. koryg.',
+            generalPrint: 'Ogólny',
+            recipe: 'Recepta'
+          },
+
+          title: {
+            invoice: 'Szczegóły faktury',
+            invoice_receipt: 'Szczegóły paragonu',
+            withdrawSlip: 'Szczegóły KW',
+            depositSlip: 'Szczegóły KP',
+            correctiveInvoice: 'Szczegóły faktury korygującej',
+            correctiveInvoice_receipt: 'Szczegóły paragonu korygującego',
+            recipe: 'Recepta'
+          },
+
+          newTitle: {
+            invoice: 'Tworzenie nowej faktury',
+            invoice_receipt: 'Tworzenie nowego paragonu',
+            withdrawSlip: 'Tworzenie nowego KW',
+            depositSlip: 'Tworzenie nowego KP',
+            correctiveInvoice: 'Tworzenie nowej faktury korygującej',
+            correctiveInvoice_receipt: 'Tworzenie nowego paragonu korygującego'
+          },
+
+          saved: {
+            invoice: 'Utworzono fakturę',
+            invoice_receipt: 'Utworzono paragon',
+            withdrawSlip: 'Utworzono KW',
+            depositSlip: 'Utworzono KP',
+            correctiveInvoice: 'Utworzono fakturę korygującą',
+            correctiveInvoice_receipt: 'Utworzono paragon korygujący'
+          },
+
+          updated: {
+            invoice: 'Zapisano zmiany faktury',
+            invoice_receipt: 'Zapisano zmiany paragonu',
+            withdrawSlip: 'Zapisano zmiany KW',
+            depositSlip: 'Zapisano zmiany KP',
+            correctiveInvoice: 'Zapisano zmiany faktury korygującej',
+            correctiveInvoice_receipt: 'Zapisano zmiany paragonu korygującego'
+          },
+
+          positions: {
+            invoice_receipt: 'Pozycje paragonu',
+            invoice: 'Pozycje faktury',
+            corrective_receipt: 'Pozycje po korekcie',
+            corrective: 'Pozycje po korekcie'
+          }
+        }
+
       });
     });
