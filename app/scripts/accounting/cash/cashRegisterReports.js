c27788332ee23bad9ddded80ef31d1dd913e80e2
Marek Romanowski <marek.romanowski@gmail.com>
2015-02-06 22:22:17 +0100

    Remove initializeSelect2.

--- app/scripts/accounting/cash/cashRegisterReports.js
+++ app/scripts/accounting/cash/cashRegisterReports.js
@@ -8,37 +8,39 @@
  * Controller of the mt.accounting
  */
 angular.module('mt.accounting')
-    .controller('CashRegisterReportsCtrl', function ($scope, $http, $routeParams, $timeout, CashRegister, CashRegisterReport) {
-      $scope.setTitle('Raporty kasowe');
+  .controller('CashRegisterReportsCtrl', function ($scope, $http, $routeParams, $timeout, CashRegister, CashRegisterReport) {
+    $scope.setTitle('Raporty kasowe');
 
-      $scope.cashRegisterReports = CashRegisterReport.query({}, $scope.filterCashReports);
+    $scope.cashRegisterReports = CashRegisterReport.query({}, $scope.filterCashReports);
 
-      $scope.cashRegisters = [{ code: 'Wszystkie' }];
-      CashRegister.query({}, function(cashRegisters) {
-        $scope.cashRegisters.pushArray(cashRegisters);
-        if ($routeParams.idCashRegister) {
-          $scope.cashRegister.value = _.find($scope.cashRegisters, function(cashRegister) {
-            return cashRegister.id === parseInt($routeParams.idCashRegister);
-          });
-        }
-      });
+    $scope.cashRegisters = [{ code: 'Wszystkie' }];
+    CashRegister.query({}, function(cashRegisters) {
+      $scope.cashRegisters.pushArray(cashRegisters);
+      if ($routeParams.idCashRegister) {
+        $scope.cashRegister.value = _.find($scope.cashRegisters, function(cashRegister) {
+          return cashRegister.id === parseInt($routeParams.idCashRegister);
+        });
+      }
+    });
 
-      $scope.filterCashReports = function (n) {
-        if (n && n.id) {
-          $scope.filteredReports = _.filter($scope.cashRegisterReports, function (report) {
-            return report.cashRegister.id === n.id;
-          });
-        } else {
-          $scope.filteredReports = $scope.cashRegisterReports;
-        }
-      };
+    $scope.filterCashReports = function (n) {
+      if (n && n.id) {
+        $scope.filteredReports = _.filter($scope.cashRegisterReports, function (report) {
+          return report.cashRegister.id === n.id;
+        });
+      } else {
+        $scope.filteredReports = $scope.cashRegisterReports;
+      }
+    };
 
-      $scope.$watch('cashRegister.value', $scope.filterCashReports);
+    $scope.$watch('cashRegister.value', $scope.filterCashReports);
 
 
-      initializeSelect2($scope, 'cashRegister', null, 'cashRegister', {
+    $scope.cashRegister = {
+      options: {
         allowClear: true,
         definedElements: $scope.cashRegisters
-      });
-      $scope.cashRegister.value = $scope.cashRegisters[0];
-    });
+      },
+      value: $scope.cashRegisters[0]
+    };
+  });
